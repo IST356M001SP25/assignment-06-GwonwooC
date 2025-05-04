91cfb64815e2378b656956a12423fac31dece97b
gcha01-su <119624169+gwonwoocha@users.noreply.github.com>
2025-05-04 18:44:55 -0400

    apicalls.py

--- tests/test_apicalls.py
+++ tests/test_apicalls.py
@@ -11,14 +11,14 @@ def test_should_pass():
     assert True
 
 
-def test_get_googe_place_details():
+def test_get_google_place_details():
     tests = [ 
-        {'place_id': 'ChIJUTtvv9Tz2YkRhneTbRT-1mk', 'expected_name': 'Buried Acorn Restaurant & Brewery'},
-        { 'place_id': 'ChIJl2h_-pjz2YkR-VUHD9dpOF0', 'expected_name': 'Meier’s Creek Brewing - Inner Harbor'},
+        {'place_id': 'ChIJUTtvv9Tz2YkRhneTbRT-1mk', 'expected_name': 'Buried Acorn Brewery'},
+        {'place_id': 'ChIJl2h_-pjz2YkR-VUHD9dpOF0', 'expected_name': 'Meier’s Creek Brewing - Inner Harbor'},
     ]
     for t in tests:
-        print(f"\nTESTING: test_get_googe_place_details({t['place_id']}) == {t['expected_name']}")
-        place =  calls.get_google_place_details(t['place_id']) 
+        print(f"\nTESTING: test_get_google_place_details({t['place_id']}) == {t['expected_name']}")
+        place = calls.get_google_place_details(t['place_id']) 
         assert place['result']['name'] == t['expected_name']
 
 
