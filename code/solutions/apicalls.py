ffd8a58c06d3d4e28149b7cf4eff0e9f24311368
gcha01-su <119624169+gwonwoocha@users.noreply.github.com>
2025-05-05 00:41:58 -0400

    reflection

--- code/solutions/apicalls.py
+++ code/solutions/apicalls.py
@@ -1,43 +1,61 @@
 import requests
 
-# Put your  CENT Ischool IoT Portal API KEY here.
-APIKEY = "APIKEYHERE"
+# Put your CENT Ischool IoT Portal API KEY here.
+APIKEY = "1ed25f224dd9347d0f0cb575"
 
 def get_google_place_details(google_place_id: str) -> dict:
-    header = { 'X-API-KEY': APIKEY }
-    params = { 'place_id': google_place_id }
+    '''
+    Takes place ID, and returns json data of details from
+    google place details API
+    '''
+    header = {"X-API-KEY": APIKEY}
+    params = {"place_id": google_place_id}
     url = "https://cent.ischool-iot.net/api/google/places/details"
-    response = requests.get(url, headers=header, params=params)
+    response = requests.get(url=url, headers=header, params=params)
     response.raise_for_status()
-    return response.json()  # Return the JSON response as a dictionary
-
-
+    return response.json()
+    
 def get_azure_sentiment(text: str) -> dict:
-    header = { 'X-API-KEY': APIKEY }
-    data = { "text" : text }
+    '''
+    Takes text string, and processes through azure sentiment analysis
+    API, returns json response
+    '''
+    header = {"X-API-KEY": APIKEY}
+    data = {"text": text}
     url = "https://cent.ischool-iot.net/api/azure/sentiment"
-    response = requests.post(url, headers=header, data=data)
+    response = requests.post(url=url, headers=header, data=data)
     response.raise_for_status()
-    return response.json()  # Return the JSON response as a dictionary
+    return response.json()
 
 def get_azure_key_phrase_extraction(text: str) -> dict:
-    header = { 'X-API-KEY': APIKEY }
-    data = { "text" : text }
+    '''
+    Takes text string, and processes through key phrase extraction
+    API, returns json response
+    '''
+    header = {"X-API-KEY": APIKEY}
+    data = {"text": text}
     url = "https://cent.ischool-iot.net/api/azure/keyphrasextraction"
-    response = requests.post(url, headers=header, data=data)
-    response.raise_for_status()
-    return response.json()  # Return the JSON response as a dictionary
+    response = requests.post(url=url, headers=header, data=data)
+    response.raise_for_status
+    return response.json()
 
 def get_azure_named_entity_recognition(text: str) -> dict:
-    header = { 'X-API-KEY': APIKEY }
-    data = { "text" : text }
+    '''
+    Takes text string, and processes through entity recognition
+    API, returns json response
+    '''
+    header = {"X-API-KEY": APIKEY}
+    data = {"text": text}
     url = "https://cent.ischool-iot.net/api/azure/entityrecognition"
-    response = requests.post(url, headers=header, data=data)
+    response = requests.post(url=url, headers=header, data=data)
     response.raise_for_status()
-    return response.json()  # Return the JSON response as a dictionary
-
+    return response.json()
 
 def geocode(place:str) -> dict:
+    '''
+    Given a place name, return the latitude and longitude of the place.
+    Written for example_etl.py
+    '''
     header = { 'X-API-KEY': APIKEY }
     params = { 'location': place }
     url = "https://cent.ischool-iot.net/api/google/geocode"
@@ -47,6 +65,10 @@ def geocode(place:str) -> dict:
 
 
 def get_weather(lat: float, lon: float) -> dict:
+    '''
+    Given a latitude and longitude, return the current weather at that location.
+    written for example_etl.py
+    '''
     header = { 'X-API-KEY': APIKEY }
     params = { 'lat': lat, 'lon': lon, 'units': 'imperial' }
     url = "https://cent.ischool-iot.net/api/weather/current"
